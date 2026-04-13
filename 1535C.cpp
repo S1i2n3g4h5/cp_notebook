fde6b9f61f35a5a109c9333b7044efba8bc23ea2
Ashdeep Singh <52857435+S1i2n3g4h5@users.noreply.github.com>
2026-04-13 13:38:42 +0530

    13/04/2026

---
+++ 1535C.cpp
@@ -0,0 +1,82 @@
+/*
+  Competing against Myself  
+  [zoASHro]
+*/
+#pragma GCC optimize("O2")  
+#include <bits/stdc++.h>
+using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+// //For making pdbs multiset with erase functionality working asexpected change `less<int>` to `less_equal<int>` and also during tree.erase using .find_by_order to remove exact value perfectly without any bugs
+template <typename... Args>
+void print(Args&&... args){
+   ((cout << args << " "), ...); cout << "\n";
+}
+#define all(x)(x).begin(),(x).end()
+#define rall(x)(x).rbegin(),(x).rend()
+#define pb push_back
+#define f(i, a, b)          for(int i = a; i < b; i++)
+#define debug(x) cout<<#x<<" = "<<x<<"\n"
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vll;
+
+
+// ______________________________________________________
+long long fast_power(long long base, long long power, long long  MOD=1e9+7ll){
+  long long result = 1; 
+  while(power > 0){
+    if(power & 1){result =(result*base) % MOD;}
+    base =(base*base) % MOD;
+    power = power>>1ll;
+  } 
+  return result;
+}
+
+
+const bool multipleTestCases = 1; 
+
+void solve(){
+  /*
+    beautiful string - alternate 01 or ? in between 0 1s
+
+  */
+
+    string s;cin>>s;
+    int n=s.size();
+
+
+    vll last(2,-1);
+    ll ans=0;
+
+    f(i,0,n){
+
+        if(s[i] == '0'){
+            last[(i%2) ^ 1] = i;
+
+        }
+        else if(s[i] == '1'){
+            last[i%2] = i;
+
+        }
+
+        ans += (i - min(last[0], last[1]));
+    }
+
+    print(ans);
+
+}
+
+
+int main(){
+  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  
+  int t = 1;
+  if(multipleTestCases) cin >> t;
+  while(t--){
+      solve();
+  }
+  return 0;
+}
