2b11671df2a77963763ddbb3bfadb71fe6c33cd0
Ashdeep Singh <52857435+S1i2n3g4h5@users.noreply.github.com>
2026-04-23 08:44:41 +0530

    morning done

---
+++ 251A.cpp
@@ -0,0 +1,89 @@
+/*
+  Competing against Myself  
+  [zoASHro]
+*/
+#pragma GCC optimize("O2")  
+#include <bits/stdc++.h>
+using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+// //For making pdbs multiset with erase functionality working asexpected change `less<int>` to `less_equal<int>` and also during tree.erase using .find_by_order to remove exact value perfectly without any bugs
+template <typename... Args>
+void print(Args&&... args){
+   ((cout << args << " "), ...); cout << "\n";
+}
+#define all(x)(x).begin(),(x).end()
+#define rall(x)(x).rbegin(),(x).rend()
+#define pb push_back
+#define f(i, a, b)          for(int i = a; i < b; i++)
+#define debug(x) cout<<#x<<" = "<<x<<"\n"
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vll;
+
+
+// ______________________________________________________
+long long fast_power(long long base, long long power, long long  MOD=1e9+7ll){
+  long long result = 1; 
+  while(power > 0){
+    if(power & 1){result =(result*base) % MOD;}
+    base =(base*base) % MOD;
+    power = power>>1ll;
+  } 
+  return result;
+}
+
+
+const bool multipleTestCases = 0; 
+
+void solve(){
+  /*
+    choosing 3 points such that 2 farthest points distance <=d
+
+    total how mayn pairs like this?
+
+
+    very imilar to just 2 pointer or sliding window type
+
+    so for every point ai we find the largest index r such that xr-xi <= d 
+    once e got all the triplet that includes ai and other 2 points will satisfy condition
+
+
+
+  */
+
+    ll n,d;cin>>n>>d;
+    vll a(n);
+    f(i,0,n)cin>>a[i];
+
+
+    ll ans=0,r=0;
+
+    f(i,0,n){
+        while(r+1<n and a[r+1] - a[i] <= d)
+            r++;
+
+        ll m = r-i;
+        if(m>=2)
+            ans += (m * (m-1))/2;
+
+    }
+
+
+    print(ans);
+
+}
+
+
+int main(){
+  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  
+  int t = 1;
+  if(multipleTestCases) cin >> t;
+  while(t--){
+      solve();
+  }
+  return 0;
+}
---
+++ 251B.cpp
@@ -0,0 +1,61 @@
+/*
+  Competing against Myself  
+  [zoASHro]
+*/
+#pragma GCC optimize("O2")  
+#include <bits/stdc++.h>
+using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+// //For making pdbs multiset with erase functionality working asexpected change `less<int>` to `less_equal<int>` and also during tree.erase using .find_by_order to remove exact value perfectly without any bugs
+template <typename... Args>
+void print(Args&&... args){
+   ((cout << args << " "), ...); cout << "\n";
+}
+#define all(x)(x).begin(),(x).end()
+#define rall(x)(x).rbegin(),(x).rend()
+#define pb push_back
+#define f(i, a, b)          for(int i = a; i < b; i++)
+#define debug(x) cout<<#x<<" = "<<x<<"\n"
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vll;
+
+
+// ______________________________________________________
+long long fast_power(long long base, long long power, long long  MOD=1e9+7ll){
+  long long result = 1; 
+  while(power > 0){
+    if(power & 1){result =(result*base) % MOD;}
+    base =(base*base) % MOD;
+    power = power>>1ll;
+  } 
+  return result;
+}
+
+
+const bool multipleTestCases = 1; 
+
+void solve(){
+  /*
+
+  */
+
+
+
+
+}
+
+
+int main(){
+  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  
+  int t = 1;
+  if(multipleTestCases) cin >> t;
+  while(t--){
+      solve();
+  }
+  return 0;
+}
